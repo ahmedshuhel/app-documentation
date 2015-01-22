9c3ef3b805f25ec9dcde2951ead4fe82d3d632f4
Rob Eisenberg <rob@bluespire.com>
2015-01-22 01:39:44 -0500

    chore(build): improve gulp watch config

--- gulpfile.js
+++ gulpfile.js
@@ -132,11 +132,14 @@ gulp.task('serve', ['build'], function(done) {
   }, done);
 });
 
+function reportChange(event){
+  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+}
+
 gulp.task('watch', ['serve'], function() {
-  var watcher = gulp.watch([path.source, path.html, path.style], ['build']);
-  watcher.on('change', function(event) {
-    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-  });
+  gulp.watch(path.source, ['build-system', browserSync.reload]).on('change', reportChange);
+  gulp.watch(path.html, ['build-html', browserSync.reload]).on('change', reportChange);
+  gulp.watch(path.style, [browserSync.reload]).on('change', reportChange);
 });
 
 gulp.task('prepare-release', function(callback){
