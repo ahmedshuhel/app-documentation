a61d3f1f7ae93fda5d96a46ae6cb066ea4ec1370
Matthew James Davis <davis.matthewjames@gmail.com>
2015-07-21 14:18:30 +0900

    feat(app): Configure router
    Added an active router for both required routes. Also updated api to inspect the router and highlight the currently displayed module. Includes some changes to styles and a refactor that includes navbar in the the app root (since it is not being used as a reusable component).

--- src/api.js
+++ src/api.js
@@ -1,13 +1,53 @@
-export class ChildRouter{
-  heading = 'Child Router';
+import { inject, computedFrom } from 'aurelia-framework';
+// import {HttpClient} from 'aurelia-http-client';
 
-  configureRouter(config, router){
-    config.map([
-      { route: ['','welcome'], name: 'welcome',      moduleId: './welcome',      nav: true, title:'Welcome' },
-      { route: 'flickr',       name: 'flickr',       moduleId: './flickr',       nav: true, title:'Flickr' },
-      { route: 'child-router', name: 'child-router', moduleId: './child-router', nav: true, title:'Child Router' }
-    ]);
+// @inject(HttpClient)
+export class TutorialViewModel{
 
-    this.router = router;
-  }
+    module = null;
+
+    // constructor(httpClient) {
+    //     this.http = httpClient;
+    // }
+
+    activate(params) {
+        if (params.module == 'binding') {
+            this.module = {
+                title: 'Binding',
+                version: '0.3.4',
+                description: 'Binding',
+                // keywords: ['di', 'dependency injection', 'ioc'],
+                // dependencies: ['aurelia-metadata','aurelia-logging'];
+                // usedby: ['aurelia-binding','aurelia-framework','aurelia-router','aurelia-templating','aurelia-templating-resources'];
+                // exports: {
+                //     classes: ['All','ClassActivator','Container','FactoryActivator','Optional','Parent','Resolver','SingletonRegistration','TransientRegistration'],
+                //     functions: ['autoinject','factory','inject','instanceActivator','registration','singleton','transient'],
+                // }
+                articles: [
+                    'Binding stuff.'
+                ]
+            };
+        } else {
+            this.module = {
+                title: 'Dependency Injection',
+                version: '0.7.1',
+                description: 'A lightweight, extensible dependency injection container for JavaScript.',
+                keywords: ['di', 'dependency injection', 'ioc'],
+                dependencies: ['aurelia-metadata','aurelia-logging'],
+                usedby: ['aurelia-binding','aurelia-framework','aurelia-router','aurelia-templating','aurelia-templating-resources'],
+                exports: {
+                    classes: ['All','ClassActivator','Container','FactoryActivator','Optional','Parent','Resolver','SingletonRegistration','TransientRegistration'],
+                    functions: ['autoinject','factory','inject','instanceActivator','registration','singleton','transient'],
+                },
+                articles: [
+                    'Using Dependency Injection in Your Classes',
+                    'Configuring the Dependency Injection Container',
+                    'Specifying Object Lifetime and Autoregistration',
+                    'Leveraging Custom Resolvers',
+                    'Creating Child Containers',
+                    'Customizing Object Creation'
+                ]
+            };
+        }
+    }
 }
--- src/app.js
+++ src/app.js
@@ -1,12 +1,31 @@
 export class App {
-  configureRouter(config, router){
-    config.title = 'Aurelia';
-    config.map([{ 
-        route: '', 
-        moduleId: './tutorials',
-        title: 'Tutorials',
-        href: '#/tutorials',
-        nav: true 
-    }]);
-  }
+  
+    configureRouter(config, router){
+        config.title = 'Aurelia';
+        config.map([{ 
+            route: ['', '/api', '/api/:module'], 
+            moduleId: './api',
+            title: 'API',
+            href: '#/api'
+        },{ 
+            route: ['/tutorial', '/tutorial/:module'], 
+            moduleId: './tutorial',
+            title: 'Tutorial',
+            href: '#/tutorial'
+        }]);
+
+        this.router = router;
+    }
+
+    get currentRoute() {
+        var route = null
+        if (this.router.currentInstruction) {
+            route = this.router.currentInstruction.fragment;
+        }
+        if (/^\/tutorial/.test(route)) {
+            return 'tutorial';
+        } else {
+            return 'api';
+        }
+    }
 }
